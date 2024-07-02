6f447de56c19f01b4e383b5cf0aa3f2cadebe934
marconi9331 <marconi.youkai@gmail.com>
2024-07-01 21:59:14 -0300

    removed amount modification on records

--- OFXtest.py
+++ OFXtest.py
@@ -3,7 +3,7 @@ import itertools as it
 from meza.io import read_csv, IterStringIO
 from csv2ofx import utils
 from csv2ofx.ofx import OFX
-from csv2ofx.mappings.stone import mapping
+#from csv2ofx.mappings.stone import mapping
 from decimal import Decimal
 from operator import itemgetter
 
@@ -23,11 +23,11 @@ def round_amount(amount, decimals):
 
 #csv_path = input("Please enter the path to the CSV file: ")
 ofx = OFX(Stone_mapping)
-records = read_csv(r"C:\teste.csv", has_header=True, delimiter=";")
+records = read_csv(r"202406_Stone_Recebimentos_parcial_DEBITO.csv", has_header=True, delimiter=";")
 
-for record in records:
-    record["VALOR LÍQUIDO"] = round_amount(record["VALOR LÍQUIDO"], 2)    
-    print(f'{record["DATA DE VENCIMENTO"]}, {record["VALOR LÍQUIDO"]}, {record["BANDEIRA"]}')
+#for record in records:
+#    record["VALOR LÍQUIDO"] = round_amount(record["VALOR LÍQUIDO"], 2)    
+#    print(f'{record["DATA DE VENCIMENTO"]}, {record["VALOR LÍQUIDO"]}, {record["BANDEIRA"]}')
 
     
 groups = ofx.gen_groups(records)
@@ -36,9 +36,11 @@ cleaned_trxns = ofx.clean_trxns(trxns)
 data = utils.gen_data(cleaned_trxns)
 content = it.chain([ofx.header(), ofx.gen_body(data), ofx.footer()])
 
-#with open(r"C:\Users\marco\Downloads\teste.ofx", "w") as f:
-#    for line in IterStringIO(content):
-#        f.write(line.__str__())
+with open(r"teste.ofx", "wb") as f:
+    for line in IterStringIO(content):
+        f.write(line)
 
 for line in IterStringIO(content):
     print(line)
+
+print("done...")
