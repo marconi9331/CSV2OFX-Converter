9604a4a4e34351e057e2b321404e1e2252fca6af
Marco <marconi.youkai@gmail.com>
2024-07-04 18:18:39 -0300

    simplified CSV_splitter

--- CSV_splitter.py
+++ CSV_splitter.py
@@ -1,9 +1,6 @@
 import csv
 
 mapFormPag = {"Crédito" : {} , "Débito" : {}}
-processed_files = []
-# Ask for user path to a CSV
-csv_path = input("Please enter the path to the CSV file: \n>")
 
 def mapTipos(tipo):
     if tipo.startswith("Crédito"):
@@ -23,12 +20,3 @@ def CSV_splitter(csv_path):
             tipo = mapTipos(row.get("TIPO"))
             addTransaction(tipo, row.get("BANDEIRA"), row)
     return mapFormPag
-
-
-# Load this CSV using CSV library
-with open(csv_path, mode='r', encoding='utf-8-sig') as file:
-    reader = csv.DictReader(file, delimiter=';', quotechar='"', doublequote=True)   
-   
-    for row in reader:
-        tipo = mapTipos(row.get("TIPO"))
-        addTransaction(tipo, row.get("BANDEIRA"), row)
